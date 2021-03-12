1c6465a64c1f5b2a6f120830ada1d88a35ab900f
AlwaleedAlroomi <alwaleed101az@gmail.com>
2021-03-12 10:32:22 +0300

    Finishing DB

--- DB_Structure.py
+++ DB_Structure.py
@@ -1,65 +1,138 @@
 from peewee import *
-
+import datetime
 # Connect to a MySQL database on network.
-mysql_db = MySQLDatabase('libraryapp', user='root', password='',
-                         host='localhost', port=3306)
+db = MySQLDatabase('libraryapp', user='root', password='',
+                   host='localhost', port=3306)
+
+BOOK_STATUS = (
+    (1, 'New'),
+    (2, 'Used'),
+    (3, 'Damaged'),
+)
+
+PURCHASE_TYPE = (
+    (1, 'Rent'),
+    (2, 'Retrieve'),
+)
+
+ACTION_TYPE = (
+    (1, 'Login'),
+    (2, 'Update'),
+    (3, 'Create'),
+    (4, 'Delete'),
+)
+
+TABLE_CHOICES = (
+    (1, 'Books'),
+    (2, 'Clients'),
+    (3, 'Employee'),
+    (4, 'Category'),
+    (5, 'Branch'),
+    (6, 'Daily_Movements'),
+    (7, 'Publisher'),
+    (8, 'Author'),
+)
+
+
+class Category(Model):
+    category_name = CharField(unique=True)
+    parent_category = IntegerField(null=True)  # Recursive relationshio
+
+    class Meta:
+        database = db
+
+
+class Publisher(Model):
+    name = CharField(unique=True)
+    Location = CharField(null=True)
+
+    class Meta:
+        database = db
+
+
+class Author(Model):
+    name = CharField(unique=True)
+    Location = CharField(null=True)
+
+    class Meta:
+        database = db
+
 
 class Books(Model):
-    title = CharField()
-    description = TextField()
-    category = ForeignKeyField(Category, backref='category')
-    code = CharField()
-    part_order = IntegerField()
-    price = DecimalField()
-    publisher = ForeignKeyField(Publisher, backref='publisher')
-    author = ForeignKeyField(Author, backref='author')
-    image = CharField()
-    status = CharField()
-    date = DateTimeField()
+    title = CharField(unique=True)
+    description = TextField(null=True)
+    category = ForeignKeyField(Category, backref='category', null=True)
+    code = CharField(null=True)
+    barcode = CharField()
+    part_order = IntegerField(null=True)
+    price = DecimalField(null=True)
+    publisher = ForeignKeyField(Publisher, backref='publisher', null=True)
+    author = ForeignKeyField(Author, backref='author', null=True)
+    image = CharField(null=True)
+    status = CharField(choices=BOOK_STATUS)  # Choices
+    date = DateTimeField(default=datetime.datetime.now)
+
+    class Meta:
+        database = db
+
 
 class Clients(Model):
     name = CharField()
-    mail = CharField()
-    phone = CharField()
-    date = DateTimeField()
-    National_ID = IntegerField()
+    mail = CharField(null=True, unique=True)
+    phone = CharField(null=True)
+    date = DateTimeField(default=datetime.datetime.now)
+    National_ID = IntegerField(null=True, unique=True)
+
+    class Meta:
+        database = db
+
 
 class Employee(Model):
     name = CharField()
-    mail = CharField()
-    phone = CharField()
-    date = DateTimeField()
-    National_ID = IntegerField()
-    periority = IntegerField()
+    mail = CharField(null=True, unique=True)
+    phone = CharField(null=True)
+    date = DateTimeField(default=datetime.datetime.now)
+    National_ID = IntegerField(null=True, unique=True)
+    periority = IntegerField(null=True)
 
-class Category(Model):
-    category_name = CharField()
-    # parent_category = Recursive relationshio
+    class Meta:
+        database = db
+
 
 class Branch(Model):
     name = CharField()
-    code = CharField()
-    location = CharField()
+    code = CharField(null=True, unique=True)
+    location = CharField(null=True)
+
+    class Meta:
+        database = db
+
 
 class Daily_Movements(Model):
     book = ForeignKeyField(Books, backref='daily_book')
     client = ForeignKeyField(Clients, backref='book_client')
-    type = CharField() # rent or retrive
+    type = CharField(choices=PURCHASE_TYPE)  # rent or retrive
     date = DateTimeField()
-    branch = ForeignKeyField(Branch, backref='Daily_branch')
-    Book_from = DateField() 
-    Book_to = DateField()
-    user = 
+    branch = ForeignKeyField(Branch, backref='Daily_branch', null=True)
+    Book_from = DateField(null=True)
+    Book_to = DateField(null=True)
+    employee = ForeignKeyField(Employee, backref='Daily_employee', null=True)
+
+    class Meta:
+        database = db
+
 
 class History(Model):
-    pass
+    employee = ForeignKeyField(Employee, backref='History_employee')
+    action = CharField(choices=ACTION_TYPE)  # Choices
+    table = CharField(choices=TABLE_CHOICES)  # Choices
+    date = DateTimeField(default=datetime.datetime.now)
+    branch = ForeignKeyField(Branch, backref='History_branch')
 
-class Publisher(Model):
-    pass
-
-class Author(Model):
-    pass
+    class Meta:
+        database = db
 
 
 db.connect()
-db.create_tables([Books, Clients, Employee, Category, Branch, Daily_Movements, History, Publisher])
+db.create_tables([Author, Category, Books, Clients, Employee, Branch,
+                  Daily_Movements, History, Publisher])
--- index.py
+++ index.py
@@ -3,158 +3,239 @@ from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
 import sys
+import MySQLdb
 
-mainui,_ = uic.loadUiType('main.ui')
+mainui, _ = uic.loadUiType('main.ui')
+
 
 class Main(QMainWindow, mainui):
     def __init__(self, parent=None):
         super(Main, self).__init__(parent)
         QMainWindow.__init__(self)
         self.setupUi(self)
+        self.DB_connect()
+        self.Handle_Buttons()
+        self.UI_changes()
+        self.Open_Daily_movements_tab()
 
     def UI_changes(self):
-        #UI changes in login
-        pass
+        # UI changes in login
+        self.tabWidget.tabBar().setVisible(False)
 
     def DB_connect(self):
-        #connection between app and db
-        pass
+        # connection between app and db
+        self.db = MySQLdb.connect(host='127.0.0.1', user='root', password='',
+                                  db='libraryapp')
+        self.cur = self.db.cursor()
+        print('done')
 
     def Handle_Buttons(self):
-        #To handle all buttons in the app
-        pass
+        # To handle all buttons in the app
+        self.pushButton.clicked.connect(self.Open_Daily_movements_tab)
+        self.pushButton_2.clicked.connect(self.Open_Books_tab)
+        self.pushButton_3.clicked.connect(self.Open_Clients_tab)
+        self.pushButton_4.clicked.connect(self.Open_Dashboard_tab)
+        self.pushButton_5.clicked.connect(self.Open_History_tab)
+        self.pushButton_6.clicked.connect(self.Open_Reports_tab)
+        self.pushButton_7.clicked.connect(self.Open_Settings_tab)
+        # Secondary buttons
+        self.pushButton_8.clicked.connect(self.Handle_Today_Work)
+        self.pushButton_19.clicked.connect(self.Add_Branch)
+        self.pushButton_21.clicked.connect(self.Add_Publisher)
+        self.pushButton_23.clicked.connect(self.Add_Author)
+        self.pushButton_24.clicked.connect(self.Add_Category)
 
     def Handle_Login(self):
-        #Handle login
+        # Handle login
         pass
 
     def Handle_Reset_Password(self):
-        #Handle reset password
+        # Handle reset password
         pass
 
     def Handle_Today_Work(self):
-        #Handle today operations 
+        # Handle today operations
         pass
-    
     ##########################################
-    #Books
+    # Books
+
     def Show_All_Books(self):
-        #To Show_All_Books
+        # To Show_All_Books
         pass
 
     def Add_New_Book(self):
-        #To Add_New_Book
+        # To Add_New_Book
         pass
 
     def Edit_Book(self):
-        #Edit_Book
+        # Edit_Book
         pass
 
     def Delete_Book(self):
-        #To Delete_Book
+        # To Delete_Book
         pass
 
     ##########################################
-    #Clients
+    # Clients
     def Show_All_Clients(self):
-        #To Show_All_Clients
+        # To Show_All_Clients
         pass
 
     def Add_New_Client(self):
-        #To Add_New_Clients
+        # To Add_New_Clients
         pass
 
     def Edit_Client(self):
-        #Edit_Client
+        # Edit_Client
         pass
 
     def Delete_Client(self):
-        #To Delete_Client
+        # To Delete_Client
         pass
 
     ##########################################
-    #History
+    # History
     def Show_History(self):
-        #Show All History to the Admin
+        # Show All History to the Admin
         pass
 
     ##########################################
-    #Books Reports
+    # Books Reports
     def All_Books_Reports(self):
-        #To All_Books_Reports
+        # To All_Books_Reports
         pass
 
     def Books_Filter_Reports(self):
-        #To Books_Filter_Reports
+        # To Books_Filter_Reports
         pass
 
     def Books_Export_Report(self):
-        #export Books Data to Excel file
+        # export Books Data to Excel file
         pass
 
     ##########################################
-    #Clients Reports
+    # Clients Reports
     def All_Clients_Reports(self):
-        #To All_Clients_Reports
+        # To All_Clients_Reports
         pass
 
     def Clients_Filter_Reports(self):
-        #To Clients_Filter_Reports
+        # To Clients_Filter_Reports
         pass
 
     def Clients_Export_Report(self):
-        #export Clients Data to Excel file
+        # export Clients Data to Excel file
         pass
 
     ##########################################
-    #Monthly Reports
+    # Monthly Reports
     def Monthly_Reports(self):
-        #To Monthly_Reports
+        # To Monthly_Reports
         pass
 
     def Monthly_Reports_Export(self):
-        #To Monthly_Reports_Export to excel file
+        # To Monthly_Reports_Export to excel file
         pass
 
     ##########################################
-    #Settings
+    # Settings
     def Add_Branch(self):
-        #To Add_Branch
-        pass
+        # To Add_Branch
+        branch_name = self.lineEdit_20.text()
+        branch_code = self.lineEdit_21.text()
+        branch_location = self.lineEdit_22.text()
+        self.cur.execute('''
+            INSERT INTO branch
+            (name, code, location)
+            VALUES
+            (%s, %s, %s)
+        ''', (branch_name, branch_code, branch_location))
+        self.db.commit()
+        print('branch added')
 
     def Add_Category(self):
-        #To Add_Category
-        pass
+        # To Add_Category
+        category_name = self.lineEdit_33.text()
+        parent_category
 
     def Add_Publisher(self):
-        #To Add_Publisher
-        pass
+        # To Add_Publisher
+        publisher_name = self.lineEdit_26.text()
+        publisher_location = self.lineEdit_28.text()
+        self.cur.execute('''
+            INSERT INTO publisher
+            (name, Location)
+            VALUES
+            (%s, %s)
+        ''', (publisher_name, publisher_location))
+        self.db.commit()
+        print('publisher added')
 
     def Add_Author(self):
-        #To Add_Author
-        pass
+        # To Add_Author
+        author_name = self.lineEdit_31.text()
+        author_location = self.lineEdit_32.text()
+        self.cur.execute('''
+            INSERT INTO author
+            (name, Location)
+            VALUES
+            (%s, %s)
+        ''', (author_name, author_location))
+        self.db.commit()
+        print('Author added')
 
     def Add_Employee(self):
-        #To Add_Employee
+        # To Add_Employee
         pass
 
     def Edit_Employee(self):
-        #To Edit_Employee
+        # To Edit_Employee
         pass
 
     def Add_Employee_Permision(self):
-        #To Add_Employee_Permision
+        # To Add_Employee_Permision
         pass
 
     def Admin_Report(self):
-        #To send Admin_Report
+        # To send Admin_Report
         pass
+
+    ###############################
+
+    def Open_Login_tab(self):
+        self.tabWidget.setCurrentIndex(0)
+
+    def Open_Reset_Password_tab(self):
+        self.tabWidget.setCurrentIndex(1)
+
+    def Open_Daily_movements_tab(self):
+        self.tabWidget.setCurrentIndex(2)
+
+    def Open_Books_tab(self):
+        self.tabWidget.setCurrentIndex(3)
+
+    def Open_Clients_tab(self):
+        self.tabWidget.setCurrentIndex(4)
+
+    def Open_Dashboard_tab(self):
+        self.tabWidget.setCurrentIndex(5)
+
+    def Open_History_tab(self):
+        self.tabWidget.setCurrentIndex(6)
+
+    def Open_Reports_tab(self):
+        self.tabWidget.setCurrentIndex(7)
+
+    def Open_Settings_tab(self):
+        self.tabWidget.setCurrentIndex(8)
+
 
 def main():
     app = QApplication(sys.argv)
     window = Main()
     window.show()
     app.exec_()
+
 
 if __name__ == '__main__':
     main()
